3cf53d9946f5e5825bbb21103e480fd5d1015527
Vinicius <viniciuszero@live.com>
2023-12-29 15:44:01 -0300

    e

--- backend/dist/src/http/controllers/errors/invalid-file-type.error.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.InvalidFileTypeError = void 0;
-class InvalidFileTypeError extends Error {
-    constructor() {
-        super('Invalid File Type');
-    }
-}
-exports.InvalidFileTypeError = InvalidFileTypeError;
